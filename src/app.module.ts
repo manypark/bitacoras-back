480f1551179319afbba4317b5387a74baccb12c8
Manu <manypark@live.com>
2022-11-04 20:44:36 -0600

    creacion modulo homework

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { HomeworkModule } from './homework/homework.module';
 
 @Module({
   imports     : [
@@ -21,6 +22,8 @@ import { UserModule } from './user/user.module';
     AuthModule,
     
     UserModule,
+    
+    HomeworkModule,
   ],
   controllers : [],
   providers   : [],
